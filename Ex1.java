921bc86defcca493f446cc8093beff18b2b93ebc
KishoreVS <33390009+KishoreVS@users.noreply.github.com>
2019-07-06 16:49:32 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,22 @@
+package lab4;
+
+import java.util.Scanner;
+
+public class Ex1 {
+public static void main(String[] args) {
+	int a;
+	int c = 0;
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter a number");
+	int n=sc.nextInt();
+	int x=n;
+	while(n>0)
+	{	
+		a=n%10;
+		n=n/10;
+		c=c+(a*a*a);
+	}
+	System.out.println("The sum of cube of the digits of the number "+x+" is:"+c);
+	sc.close();
+}
+}
